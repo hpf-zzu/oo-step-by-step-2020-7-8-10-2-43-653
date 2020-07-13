3a29b0eeee3081ac732648025927f91e0b20f791
kekeing <851575436@qq.com>
2020-07-13 23:32:16 +0800

    add by like complete class Student and test passed

--- src/main/java/com/thoughtworks/basic/Person.java
+++ src/main/java/com/thoughtworks/basic/Person.java
@@ -1,8 +1,8 @@
 package com.thoughtworks.basic;
 
 public class Person {
-    int age;
-    String name;
+    private int age;
+    private String name;
 
     public String introduce(){
         return "My name is " + this.name + ".I am " + age + " years old.";
--- src/main/java/com/thoughtworks/basic/Student.java
+++ src/main/java/com/thoughtworks/basic/Student.java
@@ -1,4 +1,23 @@
 package com.thoughtworks.basic;
 
-public class Student {
+public class Student extends Person {
+    private int classNumber;
+
+    public Student(int age, String name, int classNumber) {
+        super(age, name);
+        this.classNumber = classNumber;
+    }
+
+    public int getClassNumber() {
+        return classNumber;
+    }
+
+    public void setClassNumber(int classNumber) {
+        this.classNumber = classNumber;
+    }
+
+    @Override
+    public String introduce() {
+        return  "My name is " + getName() + ".I am " + getAge() + " years old." + "I am a Student of Class " + this.classNumber + ".";
+    }
 }
