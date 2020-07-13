a41f74294aa91ad10077ed55fa31279aac473359
kekeing <851575436@qq.com>
2020-07-13 23:55:21 +0800

    add by like write SchoolKlassTest and not passed

---
+++ src/test/java/com/thoughtworks/basic/SchoolKlassTest.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.basic;
+
+import com.sun.xml.internal.bind.v2.schemagen.episode.Klass;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class SchoolKlassTest {
+    @Test
+    public void should_contain_many_students_when_klass_new_add_student() {
+        //given
+        List<Student> students = new ArrayList<>();
+        Teacher teacher = new Teacher(30,"Matt");
+        SchoolKlass schoolKlass = new SchoolKlass(students,teacher);
+        //when
+        schoolKlass.addStudent();
+        //then
+        Assert.assertNotNull(students);
+    }
+}
