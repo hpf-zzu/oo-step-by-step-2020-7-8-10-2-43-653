bce1bf20f49144f9e9a5950e6b910cf1c12c6616
kekeing <851575436@qq.com>
2020-07-13 23:20:53 +0800

    complete Person and pass the test

---
+++ src/main/java/com/thoughtworks/basic/Person.java
@@ -0,0 +1,35 @@
+package com.thoughtworks.basic;
+
+public class Person {
+    int age;
+    String name;
+
+    public String introduce(){
+        return "My name is " + this.name + ".I am " + age + " years old.";
+    }
+
+
+    public Person() {
+    }
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
